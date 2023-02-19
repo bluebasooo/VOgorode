7833c17edc365e4a4b3f343f09891fee775b1ab1
bluebasooo <blyayachapoveshuci@yandex.ru>
2023-02-19 16:14:25 +0300

    HW1 -bugfix -docs -hw1 done

---
+++ RancherService/src/main/java/ru/tinkoff/rancher/controller/RancherController.java
@@ -0,0 +1,35 @@
+package ru.tinkoff.rancher.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/system")
+public class RancherController {
+
+    @Autowired
+    private BuildProperties properties;
+
+    /**
+     * @return status of server
+     */
+    @GetMapping("/liveness")
+    public ResponseEntity getStatus() {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    /**
+     * @return status and name of service
+     */
+    @GetMapping("/readiness")
+    public Map<String,HttpStatus> getServerStatus() {
+        return Map.of(properties.getName(), HttpStatus.OK);
+    }
+}
